9a86535d4fdbf30550bf9a83e352d43c329031e3
stanchiang <stanchiang23@gmail.com>
2014-10-01 18:43:00 -0400

    starting over with tabbed app instead of master view

---
+++ qontacts/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  qontacts
+//
+//  Created by Stanley Chiang on 10/1/14.
+//  Copyright (c) 2014 Stanley Chiang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
