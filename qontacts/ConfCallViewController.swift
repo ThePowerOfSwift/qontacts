7826a7cf5f9734b302a0c5e261d041b10eb462d8
stanchiang <stanchiang23@gmail.com>
2014-10-02 10:52:55 -0400

    renamed tabs

---
+++ qontacts/ConfCallViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ConfCallViewController.swift
+//  qontacts
+//
+//  Created by Stanley Chiang on 10/2/14.
+//  Copyright (c) 2014 Stanley Chiang. All rights reserved.
+//
+
+import Foundation
+		
